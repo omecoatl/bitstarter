e5a2e3ff42a73d786ed71a85f270cf5c786ec567
Edgar Orozco Juarez <edgar.orozco.juarez@gmail.com>
2013-07-10 01:34:52 -0500

    Added headers

--- web.js
+++ web.js
@@ -4,6 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
         var indx = fs.readFileSync('index.html');
+	response.writeHead(200, {'Content-Type': 'text/plain'});
 	response.send(indx);
 });
 
