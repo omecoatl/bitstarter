18ae526db358f6b53dbe3d387e4d568c007d3ca6
Edgar Orozco Juarez <edgar.orozco.juarez@gmail.com>
2013-07-10 01:56:26 -0500

    Headers added

--- web.js
+++ web.js
@@ -4,11 +4,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
         var indx = fs.readFileSync('index.html');
-	buf = new Buffer(256);
-	len = buf.write(indx);
-	console.log(len + " bytes: " + buf.toString('utf8', 0, len));
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.send(buf.toString());
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.end(indx);
 });
 
 var port = process.env.PORT || 5000;
